04781ceafe6e406bfea498112a753223416b5736
Isau Opeyemi AZEEZ <prodigy4442@gmail.com>
2019-03-05 14:50:26 +0100

    Adding setter and getters for Ussd

--- src/main/java/com/pc/sdpclient/model/ussd/Ussd.java
+++ src/main/java/com/pc/sdpclient/model/ussd/Ussd.java
@@ -72,6 +72,85 @@ public class Ussd implements Serializable {
     }
 
 
+
+    public Ussd(Header header, String codeScheme, Integer msgType, String serviceCode, Integer ussdOpType,
+                String ussdString, String senderCB, String receiveCB, String msisdn, Map<String, String> extension) {
+        this.header = header;
+        this.codeScheme = codeScheme;
+        this.msgType = msgType;
+        this.serviceCode = serviceCode;
+        this.ussdOpType = ussdOpType;
+        this.ussdString = ussdString;
+        this.senderCB = senderCB;
+        this.receiveCB = receiveCB;
+        this.msisdn = msisdn;
+        this.extension = extension;
+    }
+
+    public Header getHeader() {
+        return header;
+    }
+
+    public void setHeader(Header header) {
+        this.header = header;
+    }
+
+    public String getCodeScheme() {
+        return codeScheme;
+    }
+
+    public void setCodeScheme(String codeScheme) {
+        this.codeScheme = codeScheme;
+    }
+
+    public Integer getMsgType() {
+        return msgType;
+    }
+
+    public void setMsgType(Integer msgType) {
+        this.msgType = msgType;
+    }
+
+    public String getServiceCode() {
+        return serviceCode;
+    }
+
+    public void setServiceCode(String serviceCode) {
+        this.serviceCode = serviceCode;
+    }
+
+    public Integer getUssdOpType() {
+        return ussdOpType;
+    }
+
+    public void setUssdOpType(Integer ussdOpType) {
+        this.ussdOpType = ussdOpType;
+    }
+
+    public String getUssdString() {
+        return ussdString;
+    }
+
+    public void setUssdString(String ussdString) {
+        this.ussdString = ussdString;
+    }
+
+    public String getSenderCB() {
+        return senderCB;
+    }
+
+    public void setSenderCB(String senderCB) {
+        this.senderCB = senderCB;
+    }
+
+    public String getReceiveCB() {
+        return receiveCB;
+    }
+
+    public void setReceiveCB(String receiveCB) {
+        this.receiveCB = receiveCB;
+    }
+
     public String getMsisdn() {
         return msisdn;
     }
@@ -80,8 +159,38 @@ public class Ussd implements Serializable {
         this.msisdn = msisdn;
     }
 
+    public Map<String, String> getExtension() {
+        return extension;
+    }
+
+    public void setExtension(Map<String, String> extension) {
+        this.extension = extension;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ussd)) return false;
+        Ussd ussd = (Ussd) o;
+        return Objects.equals(getCodeScheme(), ussd.getCodeScheme()) && Objects.equals(getMsgType(),
+                ussd.getMsgType()) && Objects.equals(getServiceCode(), ussd.getServiceCode())
+                && Objects.equals(getUssdOpType(), ussd.getUssdOpType()) && Objects.equals(getUssdString(),
+                ussd.getUssdString()) && Objects.equals(getSenderCB(), ussd.getSenderCB())
+                && Objects.equals(getReceiveCB(), ussd.getReceiveCB()) && Objects.equals(getMsisdn(),
+                ussd.getMsisdn());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodeScheme(), getMsgType(), getServiceCode(), getUssdOpType(),
+                getUssdString(), getSenderCB(), getReceiveCB(), getMsisdn());
+    }
+
     @Override
     public String toString() {
-        return "Ussd{" + "header=" + header + ", codeScheme='" + codeScheme + '\'' + ", msgType=" + msgType + ", serviceCode='" + serviceCode + '\'' + ", ussdOpType=" + ussdOpType + ", ussdString='" + ussdString + '\'' + ", senderCB='" + senderCB + '\'' + ", receiveCB='" + receiveCB + '\'' + ", msisdn='" + msisdn + '\'' + ", extension=" + extension + '}';
+        return "Ussd{" + "header=" + header + ", codeScheme='" + codeScheme + '\'' + ", msgType="
+                + msgType + ", serviceCode='" + serviceCode + '\'' + ", ussdOpType=" + ussdOpType
+                + ", ussdString='" + ussdString + '\'' + ", senderCB='" + senderCB + '\'' + ", receiveCB='"
+                + receiveCB + '\'' + ", msisdn='" + msisdn + '\'' + ", extension=" + extension + '}';
     }
 }
